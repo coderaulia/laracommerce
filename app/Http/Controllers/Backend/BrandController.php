bf871805dabad82c731e755b055d45e1b449653a
coderaulia <auliasw89@gmail.com>
2021-09-21 23:40:16 +0700

    brand cru

--- app/Http/Controllers/Backend/BrandController.php
+++ app/Http/Controllers/Backend/BrandController.php
@@ -49,4 +49,60 @@ class BrandController extends Controller
 		return redirect()->back()->with($notification);
 
     }
+
+	 public function BrandEdit($id){
+		$brand = Brand::findOrFail($id);
+		return view('backend.brand.brand_edit',compact('brand'));
+
+	}
+
+
+	public function BrandUpdate(Request $request){
+		
+		$brand_id = $request->id;
+		$old_img = $request->old_image;
+
+		if ($request->file('brand_image')) {
+
+			unlink($old_img);
+			$image = $request->file('brand_image');
+			$name_gen = hexdec(uniqid()).'.'.$image->getClientOriginalExtension();
+			Image::make($image)->resize(300,300)->save('upload/brand/'.$name_gen);
+			$save_url = 'upload/brand/'.$name_gen;
+
+			Brand::findOrFail($brand_id)->update([
+				'brand_name_en' => $request->brand_name_en,
+				'brand_name_id' => $request->brand_name_id,
+				'brand_slug_en' => strtolower(str_replace(' ', '-',$request->brand_name_en)),
+				'brand_slug_id' => str_replace(' ', '-',$request->brand_name_id),
+				'brand_image' => $save_url,
+
+			]);
+
+			$notification = array(
+			'message' => 'Brand Updated Successfully',
+			'alert-type' => 'info'
+			);
+
+	  		return redirect()->route('all.brand')->with($notification);
+
+		} else{
+
+			Brand::findOrFail($brand_id)->update([
+				'brand_name_en' => $request->brand_name_en,
+				'brand_name_id' => $request->brand_name_id,
+				'brand_slug_en' => strtolower(str_replace(' ', '-',$request->brand_name_en)),
+				'brand_slug_id' => str_replace(' ', '-',$request->brand_name_id),
+				
+			]);
+
+			$notification = array(
+			'message' => 'Brand Updated Successfully',
+			'alert-type' => 'info'
+			);
+
+			return redirect()->route('all.brand')->with($notification);
+
+		}
+	}
 }
